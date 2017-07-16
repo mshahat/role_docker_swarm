14e5b81ccad52223551976b371116b5b9dcf4db9
Mohamed Shahat <mshahat@computer.org>
2017-07-16 13:48:48 +0100

    chef generate cookbook role_docker_swarm
    initial commit of cookbook skeleton

---
+++ metadata.rb
@@ -0,0 +1,19 @@
+name 'role_docker_swarm'
+maintainer 'The Authors'
+maintainer_email 'you@example.com'
+license 'all_rights'
+description 'Installs/Configures role_docker_swarm'
+long_description 'Installs/Configures role_docker_swarm'
+version '0.1.0'
+
+# The `issues_url` points to the location where issues for this cookbook are
+# tracked.  A `View Issues` link will be displayed on this cookbook's page when
+# uploaded to a Supermarket.
+#
+# issues_url 'https://github.com/<insert_org_here>/role_docker_swarm/issues' if respond_to?(:issues_url)
+
+# The `source_url` points to the development reposiory for this cookbook.  A
+# `View Source` link will be displayed on this cookbook's page when uploaded to
+# a Supermarket.
+#
+# source_url 'https://github.com/<insert_org_here>/role_docker_swarm' if respond_to?(:source_url)
