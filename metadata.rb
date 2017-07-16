d405af006818d8e02ac063554bf002be7cc876bb
Mohamed Shahat <mshahat@computer.org>
2017-07-16 14:35:19 +0100

    docker service, updates to kitchen.yml, metadata.rb, added rubocop.yml
    Add .rubocop.yml
    Remove .kitchen.yml supports only centos not ubuntu
    Update metadata.rb with info about repo
    default.rb now installs docker, create service and starts it

--- metadata.rb
+++ metadata.rb
@@ -1,19 +1,26 @@
 name 'role_docker_swarm'
-maintainer 'The Authors'
-maintainer_email 'you@example.com'
+maintainer 'Mohamed Shahat'
+maintainer_email 'mshahat@computer.org'
 license 'all_rights'
 description 'Installs/Configures role_docker_swarm'
 long_description 'Installs/Configures role_docker_swarm'
 version '0.1.0'
+chef_version '>=12'
+supports 'centos'
+supports 'redhat'
+
+depends 'docker', '~> 2.0'
 
 # The `issues_url` points to the location where issues for this cookbook are
 # tracked.  A `View Issues` link will be displayed on this cookbook's page when
 # uploaded to a Supermarket.
 #
 # issues_url 'https://github.com/<insert_org_here>/role_docker_swarm/issues' if respond_to?(:issues_url)
+issues_url 'https://github.com/mshahat/role_docker_swarm/issues'
 
 # The `source_url` points to the development reposiory for this cookbook.  A
 # `View Source` link will be displayed on this cookbook's page when uploaded to
 # a Supermarket.
 #
 # source_url 'https://github.com/<insert_org_here>/role_docker_swarm' if respond_to?(:source_url)
+source_url 'https://github.com/mshahat/role_docker_swarm'
--- recipes/default.rb
+++ recipes/default.rb
@@ -3,3 +3,7 @@
 # Recipe:: default
 #
 # Copyright:: 2017, The Authors, All Rights Reserved.
+
+docker_service 'default' do
+  action [:create, :start]
+end
